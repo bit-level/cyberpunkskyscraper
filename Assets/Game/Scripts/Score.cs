1a974d0ef3366400a0656ceea5983c988f599869
Andrey Kolyada <andrey.kolyada.00@gmail.com>
2020-12-14 19:35:24 +0900

    New materials in the scene

---
+++ Assets/Game/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	private Text label;
+
+	private void Awake()
+	{
+		label = GetComponent<Text>();
+	}
+
+	private void Update()
+	{
+		label.text = Skyscraper.Instance.FloorsCount.ToString();
+	}
+}
